ebe8cf6c73a52fc4b9cdd9efed8df9bcd5a5185c
janine <janine@upday.com>
2024-04-05 19:58:52 +0200

    feat: added form

--- urbanvoice/src/App.js
+++ urbanvoice/src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './screen/Home';
 import Report from './screen/Report';
+import Form from './components/Form';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
+    <ChakraProvider>
     <Router>
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/Report" element={<Report />} />
+      <Route path="/form" element={<Form />} />
       </Routes>
   </Router>
+  </ChakraProvider>
   );
 }
 
--- urbanvoice/src/screen/Report.js
+++ urbanvoice/src/screen/Report.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useRef, useCallback, useState } from 'react';
+import Camera from '../components/Camera';
+import RecordButton from '../components/RecordButton';
 
 const Report = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const handleSubmit = () => {
+    // Logic for submitting the form or performing other actions
+    setIsSubmitted(true);
+  };
+
   return (
     <div>
       <h1>Report</h1>
-      {/* Add content for the about screen */}
+      <div>
+      {isSubmitted ? null :(<><Camera/> <RecordButton /></>)}
+      <h1></h1>
+      <button onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 };
