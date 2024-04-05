21fb4cd62ee2c778f1001dc9c7560ddda9913e6e
Alam Sanchez <asancheztorres1905@gmail.com>
2024-04-05 23:11:56 +0200

    Merge main

--- urbanvoice/src/App.js
+++ urbanvoice/src/App.js
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './screen/Home';
